5c41da09c27a12dd73dc14abcd20cc34bf8a9bae
Hannah-huo <huochenhui1008@163.com>
2017-08-02 10:04:46 +0000

    tws tasks3 section2-3

--- main/section-2/practice-3.js
+++ main/section-2/practice-3.js
@@ -1,66 +1,50 @@
 'use strict';
 
 module.exports = function countSameElements(collection) {
-      var arr_SameElements = [];
-      var counts = {};
-      var specCounts = {};
-      var result = [];
-      for(var i=0;i<collection.length; i++){
+        var arr_SameElements = [];
+        var result = [];
+        for (var i = 0; i < collection.length; i++) {
             var num = collection[i];
-           if(num.indexOf('-') !== -1) {
-                 var kvPair = collection[i].split('-');
-                 result.push({name: kvPair[0], summary: kvPair[1] ? kvPair[1] : 1});
-                 /*if(kvPair.length === 2){
-                        specCounts[kvPair[0]] = parseInt(kvPair[1]);
-                 }*/
-           }else if(num.indexOf(':') !== -1){
-                 var kvPair = collection[i].split(':');
-                 result.push({name: kvPair[0], count: kvPair[1] ? kvPair[1] : 1});
-                 /*if(kvPair.length === 2){
-                        specCounts[kvPair[0]] = kvPair[1];
-                 }*/
-           }else if(num.indexOf('[') != -1){
-                 /*var k = num.substring(0,1)
-                 var v = parseInt(num.substring(num.indexOf('[')+1,num.indexOf(']')));
-                 specCounts[k]=v;*/
+            if (num.indexOf('-') !== -1) {
+                var kvPair = collection[i].split('-');
+                result.push({ name: kvPair[0], summary: kvPair[1] ? parseInt(kvPair[1]) : 1 });
+            } else if (num.indexOf(':') !== -1) {
+                var kvPair = collection[i].split(':');
+                result.push({ name: kvPair[0], summary: kvPair[1] ? parseInt(kvPair[1]) : 1 });
+            } else if (num.indexOf('[') != -1) {
                 var splitNum = num.split('');
                 var summary = parseInt(num[2] + num[3]);
                 var name = num[0];
-                result.push({name:name, summary: summary});
-           }else{
-                  //counts[num] = counts[num] ? counts[num] + 1 :1;
-                  
-                  function findInResult(result, num){
-                        result.forEach(function(element) {
-                              if(element.name === num){
-                                    return true;
-                              }
-                        }, this);
-                        return false;
-                  }
-                  //findInResult 需要自己实现
-                  if(!findInResult(result, num)) {
-                        result.push({name: num, summary: 1});
-                  } else {
-                        var k = result.find(r => num === r.name);
-                        k.summary ++;
-                  }
-           }
-      }
-      
-      return result;
-      /*var keyArray = Object.keys(counts);
-      var specArray = Object.keys(specCounts);
-      console.log(specArray);
-      for(var i = 0; i<keyArray.length;i++){
-            var count = counts[keyArray[i]];
-            for(var j =0;j<specArray.length;j++){
-                  if(keyArray[i] === specArray[j]){
-                       count +=  specCounts[specArray[j]];
-                  }
+                result.push({ name: name, summary: summary });
+            } else {
+                //findInResult 需要自己实现
+                if (!findInResult(result, num)) {
+                    result.push({ name: num, summary: 1 });
+                } else {
+                    var k = result.find(r => num === r.name);
+                    k.summary++;
+                }
             }
-            arr_SameElements.push({name:keyArray[i],summary:count})
-      }
-      return arr_SameElements;*/
-      
-}
+        }
+        //let's build together
+        var finalResult = [];
+        for(var i = 0; i< result.length; i++){
+           var k = result[i].name;
+           var v = result[i].summary;
+           if(findInResult(finalResult, k)){
+                var tobeAdded = finalResult.find(r => k === r.name);
+                tobeAdded.summary += parseInt(v);
+           }else{
+               finalResult.push({name:k , summary: v});
+           }
+        }
+        return finalResult;
+    }
+    
+    function findInResult(result, num){
+        for(var i = 0; i< result.length;i++){
+            if(result[i].name === num){
+                return true;
+            }
+        }
+    }
